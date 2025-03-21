7efb756d662765032bdf1d6d4bd2c3f9663ef7fd
Mattias Lindström <mattias.lindstrom@gmail.com>
2025-03-21 12:17:07 +0100

    Implement a basic webserver

---
+++ internal/server/server.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/mattiaslndstrm/iternaryparser/internal/api"
+)
+
+type EchoServer struct {
+	e    *echo.Echo
+	port string
+}
+
+type Server interface {
+	Start() error
+}
+
+func NewServer(port string) Server {
+	e := echo.New()
+	e.POST("/trips", tripsHandler())
+	return EchoServer{e: e, port: port}
+}
+
+func (e EchoServer) Start() error {
+	return e.e.Start(e.port)
+}
+
+func tripsHandler() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var payload [][]string
+		var err error
+		var iternary []string
+		var trips api.Trips
+		if err = c.Bind(&payload); err != nil {
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		if trips, err = validatePayload(payload); err != nil {
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		if iternary, err = api.TripsToIternary(trips); err != nil {
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		return c.JSON(http.StatusOK, iternary)
+	}
+}
+
+func validatePayload(payload [][]string) (api.Trips, error) {
+	var trips api.Trips
+	for _, item := range payload {
+		if len(item) != 2 {
+			return nil, errors.New("every trip must contain a pair of strings")
+		}
+		trips = append(trips, [2]string{item[0], item[1]})
+	}
+	return trips, nil
+}
